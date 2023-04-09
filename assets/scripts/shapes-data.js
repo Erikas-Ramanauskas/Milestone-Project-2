22ea64ee09fbea8d2d6a21ce5824a57824fbfaaf
Chaosas100 <101866007+Chaosas100@users.noreply.github.com>
2023-04-09 14:17:57 +0100

    "Added shapes dattructure, and flip function"

---
+++ assets/scripts/shapes-data.js
@@ -0,0 +1,38 @@
+const shapesArray = {
+    easy: [
+        [true],
+        [true, true, false, false],
+        [true, true, false, true],
+        [true, true, true, true],
+        [false, false, false, true, true, true, false, false, false],
+        [false, false, false, true, true, true, false, false, true],
+        [true, true, true, false, true, false, false, false, false],
+        [true, false, false, true, true, false, false, true, false],
+        [false, false, false, false, true, true, true, true, false, false, false, false, false, false, false, false],
+        [true, false, false, true]
+    ],
+    medium: [
+        [true, false, false, false, true, false, false, false, true],
+        [false, false, false, true, false, true, true, true, true],
+        [true, false, true, true, false, true, false, true, false],
+        [false, true, false, false, false, true, true, false, false, true, false, false, false, true, false, false],
+        [true, true, true, true, false, false, true, false, false],
+        [true, false, false, true, true, true, true, false, false],
+        [true, false, false, true, true, true, false, false, true],
+        [false, true, false, false, false, true, false, true, false],
+        [false, true, false, false, false, true, false, false, false, true, false, false, false, true, true, false],
+        [true, false, false, true, false, false, false, true, true]
+    ],
+    hard: [
+        [true, false, false, false, false, true, false, true, false, false, true, false, false, false, false, false],
+        [false, true, false, true, false, true, false, true, false],
+        [true, true, false, false, false, false, true, false, false, false, false, true, false, false, false, false],
+        [true, false, true, true, false, true, false, true, false],
+        [false, true, false, false, false, false, true, false, false, true, false, false, false, false, true, false],
+        [true, false, true, false, true, true, false, false, true],
+        [true, false, true, false, true, false, true, false, true],
+        [true, true, true, false, false, true, true, true, true],
+        [true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true],
+        [false, true, false, false, true, false, true, false, false, true, false, false, false, true, false, false]
+    ]
+};
