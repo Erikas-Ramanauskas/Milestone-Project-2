aaccb2fd6fdda52a720526eda5d085193b299153
Chaosas100 <101866007+Chaosas100@users.noreply.github.com>
2023-04-28 19:45:22 +0100

    Finished index page

---
+++ assets/scripts/index.js
@@ -0,0 +1,24 @@
+"use strict";
+
+//  main Js file for index.html page
+
+// reviel ections
+// const allSections = document.querySelectorAll(".section");
+
+// function revealSection(entries, observer) {
+//   const [entry] = entries;
+//   console.log(entry);
+
+//   entry.target.classList.remove("section-hidden");
+//   observer.unobserve(entry.target);
+// }
+
+// const sectionObserver = new IntersectionObserver(revealSection, {
+//   root: null,
+//   threshold: 0.15,
+// });
+
+// allSections.forEach(function (section) {
+//   section.classList.add("section-hidden");
+//   sectionObserver.observe(section);
+// });
